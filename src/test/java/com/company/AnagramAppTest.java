3ac9db70c925c60f39f917adbe7a9eb6b1bbbef8
pokal <strokachnik@gmail.com>
2021-10-21 15:45:04 +0300

    Maven Test

--- src/test/java/com/company/AnagramAppTest.java
+++ src/test/java/com/company/AnagramAppTest.java
@@ -2,10 +2,8 @@ package com.company;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.Mockito;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -14,18 +12,14 @@ import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
 class AnagramAppTest {
 
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
-    @Mock
-    private Anagram mockAnagram;
 
-    @InjectMocks
-    private AnagramApp main;
 
     @BeforeEach
     public void setUpStreams() {
@@ -34,8 +28,8 @@ class AnagramAppTest {
 
     @Test
     void startAnagramApp_shouldReadStringAndOutputReverseString_whenInputStringWithLetterAndAlphabetic() {
+        Anagram mockAnagram = Mockito.mock(Anagram.class);
         String input = "a1b2 c3d4";
-        //Anagram mockAnagram = mock(Anagram.class);
         when(mockAnagram.process(input)).thenReturn("b1a2 d3c4");
         AnagramApp.setAnagram(mockAnagram);
         InputStream targetStream = new ByteArrayInputStream(input.getBytes());
@@ -47,6 +41,7 @@ class AnagramAppTest {
     @Test
     void startAnagramApp_shouldThrowNullPointerException_whenSubmitNull() {
         String input = "a1b2 c3d4";
+        //AnagramApp.setAnagram(null);
         InputStream targetStream = new ByteArrayInputStream(input.getBytes());
         System.setIn(targetStream);
         assertThrows(NullPointerException.class, () -> AnagramApp.main(null));
